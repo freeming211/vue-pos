832f11da601716e16adc4c934189819422960327
freeming211 <228194941@qq.com>
2018-11-28 17:28:27 +0800

    Add files via upload

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="iconfont icon-xiexiangbao"></i>
+                <div>收银</div>
+            </li>
+            <li>
+                <i class="iconfont icon-zuanshi"></i>
+                <div>店铺</div>
+            </li>
+            <li>
+                <i class="iconfont icon-jiayongdianqi"></i>
+                <div>商品</div>
+            </li>
+            <li>
+                <i class="iconfont icon-31gouwuquan"></i>
+                <div>会员</div>
+            </li>
+            <li>
+                <i class="iconfont icon-shezhi"></i>
+                <div>统计</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.left-nav {
+  color: #fff;
+  font-size: 10px;
+  height: 100%;
+  background-color: #1d8ce0;
+  float: left;
+  width: 5%;
+}
+.iconfont {
+  font-size: 24px;
+}
+.left-nav ul {
+  padding: 0px;
+  margin: 0px;
+}
+.left-nav li {
+  list-style: none;
+  text-align: center;
+  border-bottom: 1px solid #20a0ff;
+  padding: 10px;
+}
+</style>
